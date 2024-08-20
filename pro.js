bfef524ffed658e6e6865638ded06a63cb2f7f26
HussainAnjan5 <muhammad.hussain.cs8@gmail.com>
2024-08-20 06:59:26 -0700

    first

---
+++ pro.js
@@ -0,0 +1,10 @@
+
+function dropdown(){
+    let droping=document.getElementById('drop');
+    if(drop.style.display!='none'){
+        drop.style.display ='none';
+    }
+    else{
+        drop.style.display='block';
+    }
+}
